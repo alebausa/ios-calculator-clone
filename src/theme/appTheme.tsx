52b598fc675d243e725dfe96f6063b646c963cdd
Ale Bausa <ale.bausa@gmail.com>
2022-10-16 16:52:52 +0200

    initial commit

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  base: {
+    backgroundColor: 'black',
+    flex: 1
+  },
+  text: {
+    color: 'white',
+    fontSize: 20
+  }
+});
